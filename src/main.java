6f3c01272d75c159d166eb253da944137a8b7844
Довгаль Степан Александрович <dovgalsa@altask.ru>
2023-01-12 10:03:50 +0700

    aa

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void  main(String[] args) {
+        System.out.println("hello wrolddlds");
+        byte a = 127;
+        byte b = 1;
+        byte result = (byte) (a+b);
+        System.out.println(result);
+
+    }
+}
